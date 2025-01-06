b75481eb658fd701dcb172a744331af1c6d6678c
Vikas8600 <vikas.moin777@gmail.com>
2025-01-06 12:05:47 +0530

    feat: add Segment and Division doctype with initial structure and tests

---
+++ chatnext_procure_to_pay/chatnext_procure_to_pay/doctype/segment/segment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Hybrowlabs and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Segment(NestedSet):
+    pass
